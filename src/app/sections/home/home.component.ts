d745771d2e6c174fbdca74be0a79f49e9320c87c
larry <larrytientcheu1@gmail.com>
2023-03-21 19:15:05 +0100

    home page done

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Posts } from 'src/app/models/posts.model';
+import { HttpServiceService } from 'src/app/services/http-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  posts! : Posts[]
+
+  constructor(private httpService: HttpServiceService){
+ }
+
+ ngOnInit(): void{
+  this.homePosts();
+ }
+
+ homePosts(){
+  this.httpService.getPosts().subscribe(
+    data => {
+      this.posts = data;
+      console.log(this.posts);
+    }
+  );
+ }
 
 }
